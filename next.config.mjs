25ec25aad21e381b928032e705719983803c06ed
skydigitaltr <ku_durmaz1@outlook.com>
2026-04-20 00:44:06 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ['lucide-react']
-  }
-};
-
-export default nextConfig;
